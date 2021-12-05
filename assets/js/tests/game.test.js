27f87053643c6421f8ac20c8442b5e123475f12f
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-12-05 17:09:47 +0800

    Add core code to the test js file

--- assets/js/tests/game.test.js
+++ assets/js/tests/game.test.js
@@ -1,3 +1,15 @@
 /**
  * @jest-environment jsdom
  */
+
+const {
+  beforeAll
+} = require("jest-circus");
+
+beforeAll(() => {
+  let fs = require("fs");
+  let fileContent = fs.readFileSync("index.html", "utf-8");
+  document.open();
+  document.write(fileContent);
+  document.close();
+})
